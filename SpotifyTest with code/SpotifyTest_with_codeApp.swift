88820160adac2deaa32a44d7bdf18cc936aa9b80
ivandemicco00 <151406012+ivandemicco00@users.noreply.github.com>
2025-11-11 11:48:45 +0100

    Initial Commit

---
+++ SpotifyTest with code/SpotifyTest_with_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyTest_with_codeApp.swift
+//  SpotifyTest with code
+//
+//  Created by Ivan De Micco on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyTest_with_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
